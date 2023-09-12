d05dc61170c22e30f20e7659a5f9c93eda61614f
objektwerks <objektwerks@runbox.com>
2023-09-12 10:07:17 -0400

    reformatted store

--- src/test/scala/homeschool/Store.scala
+++ src/test/scala/homeschool/Store.scala
@@ -1,24 +1,21 @@
 package homeschool
 
-import java.util.concurrent.TimeUnit
-
 import com.datastax.driver.core.{Cluster, SimpleStatement}
+
+import java.util.concurrent.TimeUnit
 
 import scala.io.Source
 
-class Store(url: String) {
+class Store(url: String):
   val cluster = Cluster.builder.addContactPoint(url).build()
   val asyncSession = cluster.connectAsync("event").get(10, TimeUnit.SECONDS)
   val session = cluster.connect("query")
 
-  def load(fileClasspath: String): Unit = {
+  def load(fileClasspath: String): Unit =
     val statements = Source.fromInputStream(getClass.getResourceAsStream(fileClasspath)).mkString.split(";").map(s => new SimpleStatement(s)).toIterable
     statements foreach { statement => session.execute(statement) }
-  }
 
-  def close(): Unit = {
+  def close(): Unit =
     asyncSession.close()
     session.close()
     cluster.close()
-  }
-}
